57bab2e67ea19c0adbeb3b8b17715bf73a72f7bc
Alexander Chernoshej <acharnashei@twilio.com>
2025-12-05 23:43:10 +0200

    added rerun failed in ci

--- bin/run-tests-in-gcloud.sh
+++ bin/run-tests-in-gcloud.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ ! $# -eq 5 ]; then
+if [ ! $# -eq 5 ] && [ ! $# -eq 6 ]; then
     SELF=`basename $0`
-    echo "Usage: $SELF <APK_RUNNER_APP> <APK_RUNNER_ANDROID_TEST> <RESULTS_DIR> <ARTIFACTS_DIR> <RENAME_SUFFIX>"
+    echo "Usage: $SELF <APK_RUNNER_APP> <APK_RUNNER_ANDROID_TEST> <RESULTS_DIR> <ARTIFACTS_DIR> <RENAME_SUFFIX> [TEST_TARGETS]"
     exit 1
 fi
 
@@ -11,6 +11,7 @@ APK_RUNNER_ANDROID_TEST=$2
 RESULTS_DIR=$3
 ARTIFACTS_DIR=$4
 RENAME_SUFFIX=$5
+TEST_TARGETS=$6
 
 LOG_FILE="$RESULTS_DIR/gcloud_output.txt"
 
@@ -20,18 +21,23 @@ mkdir -p "$RESULTS_DIR"
 echo "gcloud --version"
 gcloud --version
 
-gcloud firebase test android run \
+GCLOUD_CMD="gcloud firebase test android run \
         --type instrumentation \
         --timeout=30m \
-        --app "$APK_RUNNER_APP" \
-        --test "$APK_RUNNER_ANDROID_TEST" \
+        --app \"$APK_RUNNER_APP\" \
+        --test \"$APK_RUNNER_ANDROID_TEST\" \
         --device model=MediumPhone.arm,version=27,orientation=portrait \
         --device model=MediumPhone.arm,version=27,orientation=landscape \
         --device model=blueline,version=28,orientation=portrait \
         --device model=blueline,version=28,orientation=landscape \
         --device model=redfin,version=30,orientation=portrait \
-        --device model=redfin,version=30,orientation=landscape \
-        2>&1 | tee $LOG_FILE
+        --device model=redfin,version=30,orientation=landscape"
+
+if [ -n "$TEST_TARGETS" ]; then
+    GCLOUD_CMD="$GCLOUD_CMD --test-targets \"$TEST_TARGETS\""
+fi
+
+eval "$GCLOUD_CMD" 2>&1 | tee $LOG_FILE
 
 EXIT_CODE=${PIPESTATUS[0]}
 echo "gcloud finished with code $EXIT_CODE"
